fe18ec829de8f9748e4a7d36822d9460afac1a88
pog.atr1@gmail.com <pog.atr1@gmail.com>
2019-12-05 19:49:40 +0200

    Более детальная информация

--- MyData/Program.cs
+++ MyData/Program.cs
@@ -7,12 +7,27 @@ using System.Threading.Tasks;
 namespace MyData
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
-            string name = "  Poghosyan Artur Robertovich   ";
 
-            Console.WriteLine(name.Trim());
+    {
+       static void Main(string[] args)
+        {
+            string stringMyData1, stringMyDate2;
+
+             string surname = "Poghosyan";
+              string name = "Artur";
+               string patronymic = "pogosyan";
+
+            stringMyData1 = surname + " " + name +" " + patronymic;
+            
+             int age = 34;
+              double weight = 83.5;
+       
+            stringMyDate2 = " возрост " + age + " вес " + weight;
+
+            System.Console.WriteLine(stringMyData1);
+            System.Console.WriteLine(stringMyDate2);
+
+            System.Console.ReadLine();
 
         }
     }
